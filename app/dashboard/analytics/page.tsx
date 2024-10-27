056cfe1d0cea07b4f7ed36b196e04e3f3101cb1c
alvaro medrano <aljak1815@gmail.com>
2024-10-27 14:46:58 -0400

    update analitics charts

--- app/dashboard/analytics/page.tsx
+++ app/dashboard/analytics/page.tsx
@@ -1,35 +1,63 @@
 "use client";
+
 import { Label } from 'flowbite-react';
-import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
-import Chart from "react-apexcharts";
-// Cargar ReactApexChart solo en el lado del cliente
+import dynamic from 'next/dynamic';
+
+// Cargar Chart de forma dinámica
+const Chart = dynamic(() => import("react-apexcharts"), {
+    ssr: false
+});
+
+interface ChartOptions {
+    chart: {
+        type: 'line';
+        height: number;
+    };
+    xaxis: {
+        categories: number[];
+    };
+}
+
+interface SeriesData {
+    name: string;
+    data: number[];
+}
 
 export default function Analytics() {
-    const [options, setOptions] = useState({
-        chart: {
-            type: 'line' as 'line', // Asegúrate de usar el tipo literal
-            height: 350
-        },
-        xaxis: {
-            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-        }
-    });
+    const [options, setOptions] = useState<ChartOptions | null>(null);
+    const [series, setSeries] = useState<SeriesData[] | null>(null);
 
-    const [series, setSeries] = useState([{
-        name: 'sales',
-        data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-    }]);
+    useEffect(() => {
+        setOptions({
+            chart: {
+                type: 'line',
+                height: 350
+            },
+            xaxis: {
+                categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+            }
+        });
+
+        setSeries([{
+            name: 'sales',
+            data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+        }]);
+    }, []);
+
+    if (!options || !series) {
+        return <div>Cargando...</div>;
+    }
 
     return (
         <div>
             <div className='grid grid-cols-2 gap-4'>
                 <div>
-                    <Label >Registro de libros</Label>
+                    <Label>Registro de libros</Label>
                     <Chart options={options} series={series} type="line" height={250} />
                 </div>
                 <div>
-                    <Label >Libros mas solicitados</Label>
+                    <Label>Libros más solicitados</Label>
                     <Chart options={options} series={series} type="line" height={350} />
                 </div>
             </div>
