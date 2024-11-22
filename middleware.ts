5bf0188380c76e9b156c125363c39ac8ad6f82fd
alvaro medrano <aljak1815@gmail.com>
2024-11-22 14:02:33 -0400

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,36 @@
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
+const ROLE_ACCESS = {
+    dashboard: ["ADMIN", "ROOT"],
+    profile: ["ESTUDIANTE", "USUARIO EXTERNO", "DOCENTE", "ESTUDIANTIL", "COLEGIAL"]
+};
+
 export default withAuth((req) => {
     const token = req.nextauth.token;
-    if (!token) {
+    if (!token || !token.exp) {
         return NextResponse.redirect(new URL("/auth/login", req.url));
     }
-    if (!token.exp) {
-        return NextResponse.redirect(new URL("/auth/login", req.url));
-    }
+
     const currentTime = Math.floor(Date.now() / 1000);
     const tokenExpirationTime = Number(token.exp);
-    if (tokenExpirationTime && tokenExpirationTime < currentTime) {
+
+    console.log("currentTime:", currentTime);
+    console.log("tokenExpirationTime:", tokenExpirationTime);
+
+    if (tokenExpirationTime < currentTime) {
         return NextResponse.redirect(new URL("/auth/login", req.url));
     }
-    const isAdminOrRoot = token?.rols === "ADMIN" || token?.rols === "ROOT";
-    const isEstudianteOrUsuario = token?.rols === "ESTUDIANTE" || token?.rols === "USUARIO EXTERNO" || token?.rols === "DOCENTE" || token?.rols === "ESTUDIANTIL" || token?.rols === "COLEGIAL";
 
-    if (req.nextUrl.pathname.startsWith("/dashboard") && !isAdminOrRoot) {
+    const path = req.nextUrl.pathname;
+    const isAdminOrRoot = ROLE_ACCESS.dashboard.includes(token.rols as string);
+    const isEstudianteOrUsuario = ROLE_ACCESS.profile.includes(token.rols as string);
+
+    if (path.startsWith("/dashboard") && !isAdminOrRoot) {
         return NextResponse.redirect(new URL("/profile", req.url));
     }
 
-    if (req.nextUrl.pathname.startsWith("/profile") && !isEstudianteOrUsuario) {
+    if (path.startsWith("/profile") && !isEstudianteOrUsuario) {
         return NextResponse.redirect(new URL("/dashboard", req.url));
     }
 
