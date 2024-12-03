a8fb80388b2e51441566ec373621cea4796eb0e4
alvaro medrano <aljak1815@gmail.com>
2024-12-03 04:58:06 -0400

    Fix history page layout

--- app/profile/history/page.tsx
+++ app/profile/history/page.tsx
@@ -1,13 +1,12 @@
 "use client"
-import { Orders } from "@/app/dashboard/orders/Interface/Interface";
-import { ComponentPagination } from "@/components/Pagination/Pagination";
-import { ComponentSearch } from "@/components/Search/Search";
-import { InvoicesCardUserSkeleton } from "@/components/Skeleton/skeletons";
-import { Badge, Card, Select } from "flowbite-react";
+import { Orders } from "@/interface/Interface";
+import { ComponentPagination } from "@/components/Pagination";
+import { ComponentSearch } from "@/components/Search";
+import { InvoicesCardUserSkeleton } from "@/components/skeletons";
+import { Card, Select } from "flowbite-react";
 import { useEffect, useState } from "react"
-import { RiBookFill, RiCalendarLine } from "react-icons/ri";
-import { importanceColor, importanceColorMap } from "../orders/Interface/type";
 import { useRouter } from "next/navigation";
+import OrderCard from "@/components/Order/Order";
 interface SerchParams {
     searchParams: {
         query?: string;
@@ -83,38 +82,15 @@ const CardBook = ({ data, page, size }: { data: Orders[], page: number, size: nu
     }
     return (
         <div className="grid grid-cols-2 gap-4 max-sm:grid-cols-1">
-            {data.map((order, index) => {
-                const badgeColor = importanceColorMap[String(order.order_status)] || 'default';
-                const lineColor = importanceColor[String(order.order_status)] || 'default';
-                return (
-                    <Card key={index} className="relative overflow-hidden">
-                        <div className={`absolute left-0 top-0 h-full w-2 ${lineColor}`} />
-                        <div className="flex items-center justify-between">
-                            <h2 className="text-lg font-semibold">Orden #  {(page - 1) * size + index + 1}</h2>
-                            <Badge color={badgeColor} className="rounded-lg" >
-                                {order.order_status}
-                            </Badge>
-                        </div>
-                        <div className="flex items-center text-sm text-gray-600">
-                            <RiCalendarLine className="mr-2 size-4" />
-                            {new Date(order.order_at).toLocaleDateString()}
-                        </div>
-                        {order.books.map((book, index) => (
-                            <Card key={index} className="cursor-pointer " onClick={() => { handleView(Number(order.id)) }}>
-                                <div key={index} className="flex items-center">
-                                    <RiBookFill className="mr-2 size-5 text-gray-600" />
-                                    <div>
-                                        <p className="font-medium">{book.book_title_original}</p>
-                                        {book.book_title_parallel && (
-                                            <p className="text-sm text-gray-600">{book.book_title_parallel}</p>
-                                        )}
-                                    </div>
-                                </div>
-                            </Card >
-                        ))}
-                    </Card >
-                )
-            })}
+            {data.map((order, index) => (
+                <OrderCard
+                    key={index}
+                    order={order}
+                    index={index}
+                    page={page}
+                    size={size}
+                    handleView={handleView}
+                />))}
         </div >
     )
 }
