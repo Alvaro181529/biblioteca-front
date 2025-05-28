ea978831b859a06cf6cae42d08c473f74123406d
alvaro medrano <aljak1815@gmail.com>
2025-05-28 14:47:57 -0400

    added signatura topografica

--- lib/generateIA.ts
+++ lib/generateIA.ts
@@ -25,27 +25,6 @@
 //         return "No hay respuesta"
 //     }
 // }
-// export async function Signatura(titulo: string) {
-//     try {
-
-
-//         const response = await ai.models.generateContent({
-//             model: "gemini-2.0-flash",
-//             contents: `hay respuesta
-//         Eres un asistente muy preciso y respetuoso. 
-//         Por favor, responde de forma clara y concisa.  
-//         Tu tarea ahora es responder a la siguiente pregunta es cual es la signatura Dewey del libro, revista de ${titulo} solo dame el número de la signatura Dewey sin repetir el titulo ni nada solo el numero:
-
-//         Si no entiendes algo, devuelve no encontrado.
-//         Si no puedes ayudarle, dile que lo sienta y que no puede ayudarle en ese momento. 
-//     `,
-//         });
-//         return response.text
-//     } catch (error) {
-//         return "No se encontro"
-
-//     }
-// }
 'use server'
 import { BookFormData } from "@/interface/Interface";
 import { GoogleGenAI } from "@google/genai";
@@ -111,5 +90,26 @@ export async function searchBooks(query: string): Promise<BookFormData[]> {
     } catch (error) {
         console.error("Error buscando libros:", error);
         return [];
+    }
+}
+export async function Signatura(titulo: string) {
+    try {
+
+
+        const response = await ai.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: `hay respuesta
+        Eres un asistente muy preciso y respetuoso. 
+        Por favor, responde de forma clara y concisa.  
+        Tu tarea ahora es responder a la siguiente pregunta es cual es la signatura Dewey del libro, revista de ${titulo} solo dame el número de la signatura Dewey sin repetir el titulo ni nada solo el numero:
+
+        Si no entiendes algo, devuelve no encontrado.
+        Si no puedes ayudarle, dile que lo sienta y que no puede ayudarle en ese momento. 
+    `,
+        });
+        return response.text
+    } catch (error) {
+        return "No se encontro"
+
     }
 }
